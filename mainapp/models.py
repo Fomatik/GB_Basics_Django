0957e9c54f47b68afa2a75b045ece79303af51b8
Fomatik <Fomatikya@ya.ru>
2021-10-22 18:15:44 +0300

    complete lesson_3 exercise

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(verbose_name='категория', max_length=64, unique=True)
+    description = models.TextField(verbose_name='описание', blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ('id',)
+
+
+class Product(models.Model):
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, verbose_name='категория')
+    name = models.CharField(verbose_name='продукты', max_length=128)
+    image = models.ImageField(upload_to='products_images', blank=True, verbose_name='изображение')
+    short_desc = models.CharField(verbose_name='краткое описание', max_length=60, blank=True)
+    description = models.TextField(verbose_name='описание', blank=True)
+    price = models.DecimalField(verbose_name='цена', max_digits=8, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(verbose_name='количество', default=0)
+
+    def __str__(self):
+        return f'{self.name} ({self.category.name})'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
